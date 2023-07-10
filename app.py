d214be3fae9264c26f10c79264035f0356732e74
Rupantar Borphukan <94108574+Rupantar-Borphukan@users.noreply.github.com>
2023-07-10 10:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if st.button('Show Recommendation'):
         st.image(recommended_movie_posters[3])
     with col5:
         st.text(recommended_movie_names[4])
-       
+         st.image(recommended_movie_posters[4])
 
 
 
